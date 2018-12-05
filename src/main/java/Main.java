f06ceb0ac23c28f3e00a96e31aab67d5e700710c
Jakub <jakub.rola@gmail.com>
2018-12-05 10:51:13 +0100

    Added showSin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import entity.User;
 import Math.MyMath;
+import Math.Trigonometry;
 
 public class Main {
 
@@ -10,7 +11,7 @@ public class Main {
     public static void main (String[] arg){
         System.out.println(MyMath.Add(2,3));
         User kuba = new User(1,"kuba","kuba","bezrobotny");
-
+        Trigonometry.showSin();
     }
 
 }
---
+++ src/main/java/Math/Trigonometry.java
@@ -0,0 +1,11 @@
+package Math;
+
+import static java.lang.Math.PI;
+
+public class Trigonometry {
+
+    public static void showSin(){
+        System.out.println(Math.sin(PI/4));
+    }
+
+}
